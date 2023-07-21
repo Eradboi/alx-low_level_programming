49ff9bb0c4cf05cd9b54900eb3a86f39d6482d98
Erad <you@example.com>
2023-07-20 17:28:53 -0700

    digit and letter

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks through 0 and 9
+ * @c: parameter has to be checked
+ * Return: 1 on sucess and 0 on failure
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c >= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
