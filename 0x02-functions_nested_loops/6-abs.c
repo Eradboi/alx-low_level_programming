ed167b1f2aa33f7c3d5463ea973c1026f6efc0c0
Erad <you@example.com>
2023-07-23 11:05:11 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - function for absolute values
+ * @num: input
+ * Return: 0
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (num);
+	}
+}
